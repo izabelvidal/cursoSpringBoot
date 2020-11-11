e29f466cf8cd350febc47e572a42a53865c7de17
Izabel Vidal <izabelvidal99@gmail.com>
2020-11-11 13:50:15 -0300

    first commit

---
+++ src/test/java/com/izabelvidal/cursoMC/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izabelvidal.cursoMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
