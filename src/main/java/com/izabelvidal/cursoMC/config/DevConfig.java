22e0a30514680e8760730c8b46ed820863fda7e0
Izabel Vidal <izabelvidal99@gmail.com>
2020-11-18 16:28:51 -0300

    email html

--- src/main/java/com/izabelvidal/cursoMC/config/DevConfig.java
+++ src/main/java/com/izabelvidal/cursoMC/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.izabelvidal.cursoMC.services.DBService;
+import com.izabelvidal.cursoMC.services.EmailService;
+import com.izabelvidal.cursoMC.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
