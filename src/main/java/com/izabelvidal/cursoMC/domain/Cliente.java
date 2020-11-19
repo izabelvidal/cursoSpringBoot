02bdc56e46bb96d66e579b374c41efd9ff113f9d
Izabel Vidal <izabelvidal99@gmail.com>
2020-11-19 08:52:32 -0300

    adicionando senha ao cliente

--- src/main/java/com/izabelvidal/cursoMC/domain/Cliente.java
+++ src/main/java/com/izabelvidal/cursoMC/domain/Cliente.java
@@ -49,13 +49,14 @@ public class Cliente implements Serializable{
 	
 	public Cliente() {}
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.setEmail(email);
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null : tipo.getCod();
+		this.senha = senha;
 	}
 
 	public Integer getId() {
@@ -120,6 +121,14 @@ public class Cliente implements Serializable{
 
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	@Override
